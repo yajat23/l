ce4541383ef70bfad4b172261dbb848d005a82e6
yajat23 <89627077+yajat23@users.noreply.github.com>
2022-07-05 16:00:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+video = "";
+function setup()
+{
+video = createCapture(VIDEO);
+video.size(550, 500);
+
+canvas = createCanvas(550, 550);
+canvas.position(560, 150);
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+{
+    console.log(results);
+}
+}
